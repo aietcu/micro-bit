cc8fe7522553c1a4ad0c869fb4d8f755a7a83ffe
aietcu <alexandru.ietcu@gmail.com>
2018-05-11 14:39:05 +0300

    Update microBitMusicReloaded.js
    fixed application blocking when all 4 buttons are pushed

--- microBitMusicReloaded.js
+++ microBitMusicReloaded.js
@@ -1,14 +1,13 @@
 let time = 0
-let resetTime = 500
+let theEnd = false
+let resetTime = 0
 let onPin0 = false
 let onPin2 = false
 let onPin1 = false
 let melody = false
-
 input.onButtonPressed(Button.B, () => {
     melody = true
 })
-
 input.onPinPressed(TouchPin.P1, () => {
     onPin1 = true
 })
@@ -20,14 +19,16 @@ function reset() {
     onPin1 = false
     onPin2 = false
     melody = false
+    theEnd = false
 }
 input.onButtonPressed(Button.A, () => {
     onPin0 = true
 })
+resetTime = 500
 pins.analogSetPitchPin(AnalogPin.P0)
 basic.forever(() => {
     if (onPin1 || onPin2 || onPin0) {
-        for (let i = 0; i <= 20; i++) {
+        for (let i = 0; i <= 200; i++) {
             basic.pause(100)
         }
     }
@@ -39,6 +40,11 @@ basic.forever(() => {
             . . . . .
             . . . . .
             `)
+    }
+    if (onPin0 == true && onPin1 == true && onPin2 == true && melody == true) {
+        basic.showIcon(IconNames.Sad)
+        music.beginMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
+        reset()
     }
     if (onPin0 == false && onPin1 == false && onPin2 == false && melody == true) {
         basic.showLeds(`
