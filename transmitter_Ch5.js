8c75390a51f7fd951a24e94f5e5f82556885711c
aietcu <alexandru.ietcu@gmail.com>
2018-11-09 17:34:01 +0200

    create transmitter microbit move channel 5

---
+++ transmitter_Ch5.js
@@ -0,0 +1,28 @@
+
+let Button_B_state = 0
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, () => {
+    if (Button_B_state == 1) {
+        Button_B_state = 0
+    } else {
+        Button_B_state = 1
+    }
+    radio.sendValue("B", Button_B_state)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, () => {
+    if (Button_B_state == 1) {
+        Button_B_state = 0
+    } else {
+        Button_B_state = 1
+    }
+    radio.sendValue("B", Button_B_state)
+})
+radio.setGroup(5)
+basic.forever(() => {
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendValue("Pitch", input.rotation(Rotation.Pitch))
+        radio.sendValue("Roll", input.rotation(Rotation.Roll))
+    } else {
+        radio.sendValue("Pitch", 0)
+        radio.sendValue("Roll", 0)
+    }
+})
