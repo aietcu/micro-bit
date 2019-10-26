a96487db8dd807fa40ff24c37e6ad3d86296eb49
aietcu <alexandru.ietcu@gmail.com>
2019-10-26 14:02:50 +0300

    Create mearm.js
    Create custom version of mearm.js control program. Made control easier (thanks to Lucian Dusa for updating the program)

---
+++ mearm.js
@@ -0,0 +1,40 @@
+function reset22() {
+    mearm.moveToCentre(MearmServo.Base)
+    mearm.moveToCentre(MearmServo.Right)
+    mearm.moveToCentre(MearmServo.Left)
+    mearm.moveToCentre(MearmServo.Grip)
+}
+input.onButtonPressed(Button.B, function () {
+    mearm.closeGrip()
+})
+input.onButtonPressed(Button.A, function () {
+    mearm.openGrip()
+})
+input.onButtonPressed(Button.AB, function () {
+    reset22()
+})
+let speed: number
+speed = 2
+reset22()
+basic.forever(function () {
+    if (mearm.joystick(Joystick.LeftJoyX) < 0 && (mearm.joystick(Joystick.LeftJoyY) <= 5 || mearm.joystick(Joystick.LeftJoyY) >= 5)) {
+        mearm.moveByAngle(MearmServo.Base, -speed)
+    } else if (mearm.joystick(Joystick.LeftJoyX) > 0 && (mearm.joystick(Joystick.LeftJoyY) <= 5 || mearm.joystick(Joystick.LeftJoyY) >= 5)) {
+        mearm.moveByAngle(MearmServo.Base, speed)
+    }
+    if (mearm.joystick(Joystick.LeftJoyY) < 0 && (mearm.joystick(Joystick.LeftJoyX) <= 5 || mearm.joystick(Joystick.LeftJoyX) >= 5)) {
+        mearm.moveByAngle(MearmServo.Right, -speed)
+    } else if (mearm.joystick(Joystick.LeftJoyY) > 0 && (mearm.joystick(Joystick.LeftJoyX) <= 5 || mearm.joystick(Joystick.LeftJoyX) >= 5)) {
+        mearm.moveByAngle(MearmServo.Right, speed)
+    }
+    if (mearm.joystick(Joystick.RightJoyY) < 0 && (mearm.joystick(Joystick.RightJoyX) <= 5 || mearm.joystick(Joystick.RightJoyX) >= 5)) {
+        mearm.moveByAngle(MearmServo.Left, -speed)
+    } else if (mearm.joystick(Joystick.RightJoyY) > 0 && (mearm.joystick(Joystick.RightJoyX) <= 5 || mearm.joystick(Joystick.RightJoyX) >= 5)) {
+        mearm.moveByAngle(MearmServo.Left, speed)
+    }
+    if (mearm.joystick(Joystick.RightJoyX) < 0 && (mearm.joystick(Joystick.RightJoyY) <= 5 || mearm.joystick(Joystick.RightJoyY) >= 5)) {
+        mearm.moveByAngle(MearmServo.Grip, -3)
+    } else if (mearm.joystick(Joystick.RightJoyX) > 0 && (mearm.joystick(Joystick.RightJoyY) <= 5 || mearm.joystick(Joystick.RightJoyY) >= 5)) {
+        mearm.moveByAngle(MearmServo.Grip, 3)
+    }
+})
