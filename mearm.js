ee1ed7d050dfa655c1c24bd64caf4a83394029d7
aietcu <alexandru.ietcu@gmail.com>
2019-10-29 08:38:01 +0200

    Update mearm.js
    removed random speed generator and added constant for movement restriction angle

--- mearm.js
+++ mearm.js
@@ -4,6 +4,10 @@ function reset22() {
     mearm.moveToCentre(MearmServo.Left)
     mearm.moveToCentre(MearmServo.Grip)
 }
+let speed: number
+let angle: number
+speed = 2
+angle = 5
 input.onButtonPressed(Button.B, function () {
     mearm.closeGrip()
 })
@@ -11,32 +15,44 @@ input.onButtonPressed(Button.A, function () {
     mearm.openGrip()
 })
 input.onButtonPressed(Button.AB, function () {
-    //reset22()
-    speed = Math.randomRange(1, 10)
+    reset22()
 })
-let speed: number
-speed = 2
-
 reset22()
 basic.forever(function () {
-    if (mearm.joystick(Joystick.LeftJoyX) < 0 && (mearm.joystick(Joystick.LeftJoyY) <= 5 || mearm.joystick(Joystick.LeftJoyY) >= 5)) {
-        mearm.moveByAngle(MearmServo.Base, -speed)
-    } else if (mearm.joystick(Joystick.LeftJoyX) > 0 && (mearm.joystick(Joystick.LeftJoyY) <= 5 || mearm.joystick(Joystick.LeftJoyY) >= 5)) {
+    if (mearm.joystick(Joystick.LeftJoyX) < 0 && (mearm.joystick(Joystick.LeftJoyY) <= angle || mearm.joystick(Joystick.LeftJoyY) >= angle)) {
+        mearm.moveByAngle(MearmServo.Base, 0 - speed)
+    }
+    else if (mearm.joystick(Joystick.LeftJoyX) > 0 && (mearm.joystick(Joystick.LeftJoyY) <= angle || mearm.joystick(Joystick.LeftJoyY) >= angle)) {
         mearm.moveByAngle(MearmServo.Base, speed)
     }
-    if (mearm.joystick(Joystick.LeftJoyY) < 0 && (mearm.joystick(Joystick.LeftJoyX) <= 5 || mearm.joystick(Joystick.LeftJoyX) >= 5)) {
-        mearm.moveByAngle(MearmServo.Right, -speed)
-    } else if (mearm.joystick(Joystick.LeftJoyY) > 0 && (mearm.joystick(Joystick.LeftJoyX) <= 5 || mearm.joystick(Joystick.LeftJoyX) >= 5)) {
+
+    if (mearm.joystick(Joystick.LeftJoyY) < 0 && (mearm.joystick(Joystick.LeftJoyX) <= angle || mearm.joystick(Joystick.LeftJoyX) >= angle)) {
+        mearm.moveByAngle(MearmServo.Right, 0 - speed)
+    }
+    else if (mearm.joystick(Joystick.LeftJoyY) > 0 && (mearm.joystick(Joystick.LeftJoyX) <= angle || mearm.joystick(Joystick.LeftJoyX) >= angle)) {
         mearm.moveByAngle(MearmServo.Right, speed)
     }
-    if (mearm.joystick(Joystick.RightJoyY) < 0 && (mearm.joystick(Joystick.RightJoyX) <= 5 || mearm.joystick(Joystick.RightJoyX) >= 5)) {
-        mearm.moveByAngle(MearmServo.Left, -speed)
-    } else if (mearm.joystick(Joystick.RightJoyY) > 0 && (mearm.joystick(Joystick.RightJoyX) <= 5 || mearm.joystick(Joystick.RightJoyX) >= 5)) {
+    else if (mearm.joystick(Joystick.RightJoyY) < 0 && (mearm.joystick(Joystick.RightJoyX) <= angle || mearm.joystick(Joystick.RightJoyX) >= angle)) {
+        mearm.moveByAngle(MearmServo.Left, 0 - speed)
+    }
+    else if (mearm.joystick(Joystick.RightJoyY) > 0 && (mearm.joystick(Joystick.RightJoyX) <= angle || mearm.joystick(Joystick.RightJoyX) >= angle)) {
         mearm.moveByAngle(MearmServo.Left, speed)
     }
-    if (mearm.joystick(Joystick.RightJoyX) < 0 && (mearm.joystick(Joystick.RightJoyY) <= 5 || mearm.joystick(Joystick.RightJoyY) >= 5)) {
-        mearm.moveByAngle(MearmServo.Grip, -3)
-    } else if (mearm.joystick(Joystick.RightJoyX) > 0 && (mearm.joystick(Joystick.RightJoyY) <= 5 || mearm.joystick(Joystick.RightJoyY) >= 5)) {
-        mearm.moveByAngle(MearmServo.Grip, 3)
+    else if (mearm.joystick(Joystick.RightJoyX) < 0 && (mearm.joystick(Joystick.RightJoyY) <= angle || mearm.joystick(Joystick.RightJoyY) >= angle)) {
+        mearm.moveByAngle(MearmServo.Grip, -1)
     }
+    else if (mearm.joystick(Joystick.RightJoyX) > 0 && (mearm.joystick(Joystick.RightJoyY) <= angle || mearm.joystick(Joystick.RightJoyY) >= angle)) {
+        mearm.moveByAngle(MearmServo.Grip, 1)
+    }
+    else if (mearm.joystick(Joystick.RightJoyX) > 0 && mearm.joystick(Joystick.RightJoyY) > 0 ||
+        mearm.joystick(Joystick.RightJoyX) > 0 && mearm.joystick(Joystick.RightJoyY) < 0 ||
+        mearm.joystick(Joystick.RightJoyX) < 0 && mearm.joystick(Joystick.RightJoyY) > 0 ||
+        mearm.joystick(Joystick.RightJoyX) < 0 && mearm.joystick(Joystick.RightJoyY) < 0 ||
+        mearm.joystick(Joystick.LeftJoyX) > 0 && mearm.joystick(Joystick.LeftJoyY) > 0 ||
+        mearm.joystick(Joystick.LeftJoyX) > 0 && mearm.joystick(Joystick.LeftJoyY) < 0 ||
+        mearm.joystick(Joystick.LeftJoyX) < 0 && mearm.joystick(Joystick.LeftJoyY) > 0 ||
+        mearm.joystick(Joystick.LeftJoyX) < 0 && mearm.joystick(Joystick.LeftJoyY) < 0)
+    { }
+
+    basic.pause(1)
 })
